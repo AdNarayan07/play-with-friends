dd7fd6689f69045b9b119c9741bd5354564f59bf
AdNarayan <aditya6104narayan@gmail.com>
2024-05-13 11:56:36 +0530

    ..

--- frontend/pages/mickeyMouseDonaldDuck/assets/script.js
+++ frontend/pages/mickeyMouseDonaldDuck/assets/script.js
@@ -19,7 +19,7 @@ createRoom.addEventListener('submit', (e)=>{
     e.preventDefault()
     let roomName = document.querySelector('#createRoom > input').value
     if(!roomName) return alert('Please enter a name for your room')
-    let data =  { user, roomID: uuidv4(), roomName, max: 3, parent: 'mickeyMouseDonaldDuck', game: { players: [], gameActive: false, roundActive: false, rounds: 10, currentRound: 0, cards: { Mickey: null, Mouse: null, Donald: null, Duck: null } } }
+    let data =  { user, roomID: uuidv4(), roomName, max: 3, parent: 'mickeyMouseDonaldDuck', game: { players: [], scores: {}, rounds: [], gameActive: false, roundActive: false, maxRounds: 10, currentRound: 1, cards: { Mickey: null, Mouse: null, Donald: null, Duck: null } } }
     socket.emit('createRoom', data)
     location.href = '/mickeyMouseDonaldDuck/room?id=' + data.roomID
 })
